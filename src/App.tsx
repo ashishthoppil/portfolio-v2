9b3cb9fe4ec598e1639eba0d5ad9cccbf9419283
Ashish <ashishthoppil@gmail.com>
2024-01-24 23:59:05 +0530

    shadcn added, hero section in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/layout/Header'
+import { motion } from "framer-motion"
 
 function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+      <div  className='h-screen grid grid-cols-2 px-[5rem] items-center'>
+        <motion.div
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        transition={{ duration: 2.5 }}
+         className='grid gap-[5rem]'>
+          <h1 className='text-9xl'>Hey there!</h1>
+          <h3 className='text-7xl'>I'm Ashish, a Frontend Developer.</h3>
+        </motion.div>
+        <div className=''>
+        
+          
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,21 @@
 import logo from '../../assets/images/logo-light.png';
+import { Button } from "@/components/ui/button"
+import { DownloadIcon } from "@radix-ui/react-icons"
 
 export const Header = () => {
     return (
         <header className='flex justify-between px-[5rem] shadow'>
             <img className='h-[6rem]' src={logo} />
             <nav className='flex items-center'>
-                <ul className='flex gap-x-5'>
+                <ul className='flex gap-x-[4rem] items-center'>
                     <li>Home</li>
                     <li>Experience</li>
                     <li>Projects</li>
+                    <li>    
+                        <Button>
+                            <DownloadIcon className="mr-2 h-4 w-4" /> Resume
+                        </Button>
+                    </li>
                 </ul>
             </nav>
         </header>
