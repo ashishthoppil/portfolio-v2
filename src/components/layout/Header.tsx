c4782a540b5392acf48a1bb8260acb5df9c5a269
Ashish <ashishthoppil@gmail.com>
2024-01-26 00:12:35 +0530

    Hero section updates

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,23 @@
-import logo from '../../assets/images/logo-light.png';
-import { Button } from "@/components/ui/button"
-import { DownloadIcon } from "@radix-ui/react-icons"
+import logo from "../../assets/images/logo-light.png";
+import { Button } from "@/components/ui/button";
+import { DownloadIcon } from "@radix-ui/react-icons";
 
 export const Header = () => {
-    return (
-        <header className='flex justify-between px-[5rem] shadow'>
-            <img className='h-[6rem]' src={logo} />
-            <nav className='flex items-center'>
-                <ul className='flex gap-x-[4rem] items-center'>
-                    <li>Home</li>
-                    <li>Experience</li>
-                    <li>Projects</li>
-                    <li>    
-                        <Button>
-                            <DownloadIcon className="mr-2 h-4 w-4" /> Resume
-                        </Button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="flex justify-between px-[5rem] shadow">
+      <img className="h-[6rem]" src={logo} />
+      <nav className="flex items-center">
+        <ul className="flex gap-x-[4rem] items-center">
+          <li>Home</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>
+            <Button className="hover:scale-105 transition duration-400">
+              <DownloadIcon className="mr-2 h-4 w-4" /> Resume
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
